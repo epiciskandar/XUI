88461a159c90c4edfc76cf008fb68a8ae35cf118
iceable <iceable@foxmail.com>
2013-03-27 22:44:08 +0800

    添加Static对FontName的支持

--- XUI/XUI/XFrameWork/XCtrls/XStatic.hpp
+++ XUI/XUI/XFrameWork/XCtrls/XStatic.hpp
@@ -8,6 +8,7 @@ class CXStatic : public CXElement
 	XProperty_Begin
 		XProperty(Text)
 		XProperty(TextColor)
+        XProperty(FontName)
 	XProperty_End;
 
 	virtual XResult SetXMLProperty( CString name,CString value );
@@ -44,12 +45,32 @@ inline VOID CXStatic::On_CXMsg_Paint(CXMsg_Paint& msg)
 	rect.OffsetRect(msg.offsetFix);
 	CString text;
 	GetText(text);
+    CString fontName;
+    GetFontName (fontName);
+
 	if (msg.drawDevice.IsRectNeedRePaint(rect))
 	{
 		COLORREF color;
 		GetColor(color);
 		CPen pen;
-		pen.CreatePen(PS_SOLID,1,color);
+        pen.CreatePen(PS_SOLID,1,color);
+        
+        if (fontName != _T(""))
+        {
+            /* ÐÞ¸Ä×ÖÌå */
+            LOGFONT lf = {0};
+            CFont font(msg.drawDevice.dc.GetCurrentFont());
+            CFont newFont;
+            font.GetLogFont (&lf);
+            lf.lfCharSet = 134;
+            lf.lfHeight = -150;
+            lf.lfWidth = 0;
+            lstrcpy (lf.lfFaceName, fontName);
+            newFont.CreateFontIndirect (&lf);
+            msg.drawDevice.dc.SelectFont (newFont.m_hFont);
+         }
+
+
 		CGDIHandleSwitcher switcher(msg.drawDevice.dc,pen,FALSE);
 		DRAWTEXTPARAMS params;
 		ZeroMemory(&params,sizeof(params));
