8a127f8532938fcfd2870ee8621ac28ef650f598
epiciskandar@gmail.com <epiciskandar@gmail.com>
2013-04-16 21:07:27 +0800

    重定向中间文件，减少根目录文件夹数量

--- XUI/postbuild.bat
+++ XUI/postbuild.bat
@@ -9,8 +9,8 @@ xcopy /f/c/y "./XUI/WTL/*.h" "../output/Lib_%1/include/WTL/" > nul
 
 del /f/q "../output/Lib_%1/dll" > nul
 mkdir "../output/Lib_%1/dll" > nul
-xcopy /f/c/y "../%1/*.dll" "../output/Lib_%1/dll" > nul
-xcopy /f/c/y "../%1/*.lib" "../output/Lib_%1/dll" > nul
+xcopy /f/c/y "../output/%1/*.dll" "../output/Lib_%1/dll" > nul
+xcopy /f/c/y "../output/%1/*.lib" "../output/Lib_%1/dll" > nul
 
 if %1==Release goto release
 xcopy /f/c/y "VisualLeakDetector/bin/win32/*" "../%1" > nul
