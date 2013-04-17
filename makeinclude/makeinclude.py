eb3baab9f6252663f820075df2c3d3b542017aa8
iceable <iceable@foxmail.com>
2013-04-17 17:46:57 +0800

    添加makeinclude工具

---
+++ makeinclude/makeinclude.py
@@ -0,0 +1,47 @@
+import os.path
+
+list = []
+
+def Parse(target="XUI_include.h"):
+	n = 0
+	for n in range(len(list)):
+		if list[n] == target:
+			return "\n"
+	list.append(target)
+	
+	if os.path.exists(target) == True:
+		f = open(target,"r")
+		lines = f.readlines();
+		f.close()
+		print("Read file : " +target + " success\n")
+		i = 0
+		file = ""
+		temp = ""
+		for i in range(len(lines)):
+			line = lines[i]
+			#include current directory file will be parse
+			if "#include \"" in line and not "\\" in line and not "/" in line:
+				file = line[10:-2]
+				if os.path.exists(file) == False:
+					print("Can't find "+ file +"\n")
+					continue
+				lines[i] = Parse(file)
+			#delete '#pragma once'
+			if "#pragma once" in line:
+				lines[i] = "\n"
+			temp += lines[i]
+		return temp
+	else:
+		print("Can't find "+target+"\n")
+		return ""
+
+if __name__ == "__main__":
+	i = Parse()
+	if i != "":
+		f = open("XUI_include.h.new", "w")
+		f.write(i)
+		f.close()
+		print("Sava as XUI_include.h.new Complete!")
+	else:
+		print("Failed!")
+	
